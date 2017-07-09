02788b12d243ba9aca37aee355fe62c1ffa6b306
Andreas Göransson <andreasgoransson0@gmail.com>
2017-07-09 08:55:55 +0200

    Added some debug printing, to understand why sed cannot find the file.

--- travis_prebuild.sh
+++ travis_prebuild.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+search_dir=$PWD
+work_dir=$PWD
+find "${search_dir}" "${work_dir}" -mindepth 1 -maxdepth 1 -type f -print0 | xargs -0 -I {} echo "{}"
+
 VERSIONVAL=master
 
 # Use tag as version
