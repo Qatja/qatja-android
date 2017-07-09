e4d5c8b8f7eb51110bd49a67a13f1b97277d8ea9
Andreas Göransson <andreasgoransson0@gmail.com>
2017-07-09 09:08:03 +0200

    Added jdk to travis script.

--- travis_prebuild.sh
+++ travis_prebuild.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-
-search_dir=$PWD
-work_dir=$PWD
-find "${search_dir}" "${work_dir}" -mindepth 1 -maxdepth 1 -type f -print0 | xargs -0 -I {} echo "{}"
-
 VERSIONVAL=master
 
 # Use tag as version
@@ -14,4 +9,4 @@ else
   VERSIONVAL="dev"
 fi
 
-sed -i -e "s/@VERSION@/$VERSIONVAL/g" ./gradle.properties
+sed -i -e "s/@VERSION@/$VERSIONVAL/g" gradle.properties
